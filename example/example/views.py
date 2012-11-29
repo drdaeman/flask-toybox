6aa22e574cb1bc91343edd404f60ddb1d1a23326
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2012-11-29 16:52:25 +0400

    Updated example to use saCollectionView

--- example/example/views.py
+++ example/example/views.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from flask.ext.toybox.sqlalchemy import saModelView
+from flask.ext.toybox.sqlalchemy import saModelView, saCollectionView
 from flask.ext.toybox.views import BaseModelView
 from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.exceptions import NotFound
@@ -9,37 +9,28 @@ from . import models
 
 
 SAModelView = saModelView(db.session)
+SACollectionView = saCollectionView(db.session)
 
 
-class UserSubmodelView(SAModelView):
+class UserSubmodelMixin(object):
     def get_query(self, **kwargs):
         username = kwargs.pop("username")
-        q = super(UserSubmodelView, self).get_query(**kwargs)
+        q = super(UserSubmodelMixin, self).get_query(**kwargs)
         return q.filter(self.model.user.has(username=username))
-
-class UserSubmodelsView(BaseModelView):
-    def fetch_object(self, username):
-        try:
-            user = db.session.query(models.User).filter_by(username=username).one()
-        except NoResultFound:
-            raise NotFound
-        return db.session.query(self.model).filter_by(user=user).all()
 
 
 @class_route("/", name="users")
-class UsersView(BaseModelView):
-    def fetch_object(self):
-        q = db.session.query(models.User).values(models.User.username)
-        return [row.username for row in q]
+class UsersView(SACollectionView):
+    model = models.User
 
 @class_route("/~<username>/", name="user")
 class UserView(SAModelView):
     model = models.User
 
 @class_route("/~<username>/posts/", name="user_posts")
-class PostsView(UserSubmodelsView):
+class PostsView(UserSubmodelMixin, SACollectionView):
     model = models.Post
 
 @class_route("/~<username>/posts/<id>", name="user_post")
-class PostView(UserSubmodelView):
+class PostView(UserSubmodelMixin, SAModelView):
     model = models.Post
