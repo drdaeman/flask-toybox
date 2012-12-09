c54bca55a4b0be4f1b2be7bda5ae5cdb215959ed
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2012-12-09 18:53:08 +0400

    Exclude fallback from coverage reporting

--- flask_toybox/compat.py
+++ flask_toybox/compat.py
@@ -5,5 +5,5 @@ Cross-version compatibility module.
 from __future__ import absolute_import
 try:
     from collections import OrderedDict
-except ImportError:
+except ImportError: # pragma: no cover
     from ordereddict import OrderedDict
